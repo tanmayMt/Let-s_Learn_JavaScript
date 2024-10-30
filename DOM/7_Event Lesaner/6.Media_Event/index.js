8af7cd33f7aa916a20a7c188beb0c6c7431037ad
web-collab <tanmoy587@gmail.com>
2024-10-30 07:55:32 +0530

    Media(audio or video ) Event

---
+++ DOM/7_Event Lesaner/6.Media_Event/index.js
@@ -0,0 +1,26 @@
+//  audio / video events
+// canplay, play, playing, pause, progress, ended, volumechange, waiting
+
+const video = document.querySelector("video");
+// console.log(video)
+
+video.addEventListener("canplay", function () {
+  console.log("canplay");
+});
+
+video.addEventListener("play", function () {
+  console.log("play");
+});
+video.addEventListener("playing", function () {
+  console.log("playing");
+});
+
+video.addEventListener("pause", function () {
+  console.log("pause");
+});
+video.addEventListener("ended", function () {
+  console.log("Thanks for watching");
+});
+video.addEventListener("volumechange", function () {
+  console.log("volumechange");
+});
