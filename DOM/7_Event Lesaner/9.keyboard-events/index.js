db76fbc61d08f8f0c653ac21e214919e8d57286f
web-collab <tanmoy587@gmail.com>
2024-11-05 10:22:51 +0530

    clipboard-evets

--- DOM/7_Event Lesaner/9.keyboard-events/index.js
+++ DOM/7_Event Lesaner/9.keyboard-events/index.js
@@ -4,21 +4,21 @@
 // 3. keyup
 // some properties - key, keyCode, code, shiftKey, ctlKey, repeat
 
-// const textarea = document.querySelector("textarea");
-// textarea.addEventListener("keydown", function (e) {
-//   console.log(e.key)
-//   if (e.repeat) {
-//     alert("Warning! Please do not repeat");
-//   }
-// });
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keydown", function (e) {
+  console.log(e.key)
+  if (e.repeat) {
+    alert("Warning! Please do not repeat");
+  }
+});
 
 // textarea.addEventListener("keypress", function () {
 //   console.log("keypress");
 // });
 
-textarea.addEventListener("keyup", function (e) {
-  console.log("keyup")
-  if (e.shiftKey) {
-    console.log("shift+" + e.key);
-  }
-});
+// textarea.addEventListener("keyup", function (e) {
+//   console.log("keyup")
+//   if (e.shiftKey) {
+//     console.log("shift+" + e.key);
+//   }
+// });
