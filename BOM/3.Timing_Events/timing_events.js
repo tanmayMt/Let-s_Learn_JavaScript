3b2636fbc059235cb77e495c10baabf336f10529
web-collab <tanmoy587@gmail.com>
2024-11-06 08:48:04 +0530

    Time Events (use setInterval() to make dynamic clock)

--- BOM/3.Timing_Events/timing_events.js
+++ BOM/3.Timing_Events/timing_events.js
@@ -17,7 +17,7 @@
 
 
 
-
+//Display "user registration successful" <- this messege for 1 second by clicking save button
 const saveButton = document.querySelector(".save-btn");
 const message1 = document.querySelector(".message1");
 
@@ -32,9 +32,10 @@ function saveUser() {
 
 
 
-
-startCountingButton=document.querySelector(".start-counting-btn");
+//Click button for counting
+const startCountingButton=document.querySelector(".start-counting-btn");
 const message2 = document.querySelector(".message2");
+
 startCountingButton.addEventListener("click", displayCount);
 function displayCount() {
   let count = 1;
@@ -46,3 +47,32 @@ function displayCount() {
   }, 1000);
 }
 
+
+
+//Dynamic Clock
+const sartClockButton=document.querySelector(".start-clock-btn");
+const message3 = document.querySelector(".message3");
+
+sartClockButton.addEventListener("click", startClock);
+function startClock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  minutes = formatTime(minutes);
+  seconds = formatTime(seconds);
+
+  let time = hours + ":" + minutes + ":" + seconds;
+
+  message3.textContent = time;
+
+  setInterval(startClock, 1000);
+}
+
+function formatTime(value) {
+  if (value < 10) {
+    value = "0" + value;
+  }
+  return value;
+}
