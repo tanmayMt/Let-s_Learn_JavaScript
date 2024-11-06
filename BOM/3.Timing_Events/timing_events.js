6708ba55b5ddc73be4c254626a5c0adb069d968a
web-collab <tanmoy587@gmail.com>
2024-11-06 08:29:18 +0530

    Time Events (setInterval() in real time example)

--- BOM/3.Timing_Events/timing_events.js
+++ BOM/3.Timing_Events/timing_events.js
@@ -19,13 +19,57 @@
 
 
 const saveButton = document.querySelector(".save-btn");
-const message = document.querySelector(".message");
+const message1 = document.querySelector(".message1");
 
 saveButton.addEventListener("click", saveUser);
 function saveUser() {
-  message.textContent = "user registration successful";
+  message1.textContent = "user registration successful";
 
   setTimeout(() => {
-    message.textContent = "";
+    message1.textContent = "";
   }, 2000);
 }
+
+
+
+
+countClockButton=document.querySelector(".count-clock-btn");
+const message2 = document.querySelector(".message2");
+countClockButton.addEventListener("click", displayCount);
+function displayCount() {
+  let count = 1;
+  message2.textContent = count;
+
+  setInterval(() => {
+    count++;
+    message2.textContent = count;
+  }, 1000);
+}
+
+
+
+////Dynamic Clock
+// saveButton.addEventListener("click", startClock);
+
+// function startClock() {
+//   let date = new Date();
+//   let hours = date.getHours();
+//   let minutes = date.getMinutes();
+//   let seconds = date.getSeconds();
+
+//   minutes = formatTime(minutes);
+//   seconds = formatTime(seconds);
+
+//   let time = hours + ":" + minutes + ":" + seconds;
+
+//   message.textContent = time;
+
+//   setInterval(startClock, 1000);
+// }
+
+// function formatTime(value) {
+//   if (value < 10) {
+//     value = "0" + value;
+//   }
+//   return value;
+// }
