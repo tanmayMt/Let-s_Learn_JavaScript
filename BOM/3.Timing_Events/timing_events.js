b4cbe3a0d420fb0f88e0aa8710d97e51e52dd931
web-collab <tanmoy587@gmail.com>
2024-11-06 08:09:27 +0530

    Time Events (setTimeout())

--- BOM/3.Timing_Events/timing_events.js
+++ BOM/3.Timing_Events/timing_events.js
@@ -3,71 +3,15 @@
 // setTimeOut(), setInterval()
 
 
-setTimeout(() => {
-  console.log("hi");
-}, 3000);
+// setTimeout(() => {
+//   console.log("hi");
+// }, 3000);
 
-// setTimeout(display, 2000);
+setTimeout(display, 2000);
 
-// function display() {
-//   console.log("display function");
-// }
-
-
+function display() {
+  console.log("display function");
+}
 
 
 
-// const saveButton = document.querySelector(".save-btn");
-// const message = document.querySelector(".message");
-
-// saveButton.addEventListener("click", saveUser);
-// function saveUser() {
-//   message.textContent = "user registration successful";
-
-//   setTimeout(() => {
-//     message.textContent = "";
-//   }, 2000);
-// }
-
-
-
-
-
-// saveButton.addEventListener("click", displayCount);
-// function displayCount() {
-//   let count = 1;
-//   message.textContent = count;
-
-//   setInterval(() => {
-//     count++;
-//     message.textContent = count;
-//   }, 1000);
-// }
-
-
-
-////Dynamic Clock
-// saveButton.addEventListener("click", startClock);
-
-// function startClock() {
-//   let date = new Date();
-//   let hours = date.getHours();
-//   let minutes = date.getMinutes();
-//   let seconds = date.getSeconds();
-
-//   minutes = formatTime(minutes);
-//   seconds = formatTime(seconds);
-
-//   let time = hours + ":" + minutes + ":" + seconds;
-
-//   message.textContent = time;
-
-//   setInterval(startClock, 1000);
-// }
-
-// function formatTime(value) {
-//   if (value < 10) {
-//     value = "0" + value;
-//   }
-//   return value;
-// }
