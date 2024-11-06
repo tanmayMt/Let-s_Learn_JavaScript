c7dbac41a1e226e8d65438461340cf6d85768ddd
web-collab <tanmoy587@gmail.com>
2024-11-06 08:16:36 +0530

    Time Events (setTimeout() in real time example)

--- BOM/3.Timing_Events/timing_events.js
+++ BOM/3.Timing_Events/timing_events.js
@@ -7,10 +7,25 @@
 //   console.log("hi");
 // }, 3000);
 
-function display() {
-  console.log("display function");
-}
+//function display() {
+//  console.log("display function");
+//}
 
 // setTimeout(display, 2000);
-setInterval(display, 2000);
+// setInterval(display, 2000);
 
+
+
+
+
+const saveButton = document.querySelector(".save-btn");
+const message = document.querySelector(".message");
+
+saveButton.addEventListener("click", saveUser);
+function saveUser() {
+  message.textContent = "user registration successful";
+
+  setTimeout(() => {
+    message.textContent = "";
+  }, 2000);
+}
