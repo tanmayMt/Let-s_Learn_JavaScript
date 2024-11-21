38acdb33c153c78487c6bac3651c718d0eb7b47e
web-collab <tanmoy587@gmail.com>
2024-11-21 17:14:44 +0530

    Callback function and Higher order function

--- ES6/8_ASYNCHRONOUS_PROG/index2.js
+++ ES6/8_ASYNCHRONOUS_PROG/index2.js
@@ -5,5 +5,15 @@ const printSquare = (x) => {
   console.log(`The square is ${x * x}`);
 };
 
-const square = printSquare;
-square(10);
+// const square = printSquare;
+// square(10);
+
+
+// When a function "higherOrderFunction" recives another function "callback" as an argument, then that function is called Higher order function.
+// And the function "callback" which is recived as argument in a function "higherOrderFunction" is called Callback function.
+const higherOrderFunction = (y, callback) => { 
+  callback(y);
+};
+
+// higherOrderFunction(5, square);   
+higherOrderFunction(5, printSquare);  
