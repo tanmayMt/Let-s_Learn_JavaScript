fe322da06ba5195339b15675f1d26ae5a32f0352
web-collab <tanmoy587@gmail.com>
2024-11-24 19:25:20 +0530

    Asynchronous operations, triggered by events

---
+++ ES6/8_ASYNCHRONOUS_PROG/index4.js
@@ -0,0 +1,9 @@
+//How all Even lesanter work as async function
+
+console.log("Hi");
+
+document.querySelector("button").addEventListener("click",()=>{
+    console.log("Button is click");
+})
+
+console.log("Bye");
