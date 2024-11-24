6757be6f35dc3d02a402e5d91d7d4d403ba8b5fc
web-collab <tanmoy587@gmail.com>
2024-11-24 19:07:55 +0530

    Asynchronous Task Execution with Callbacks

--- ES6/8_ASYNCHRONOUS_PROG/index3.js
+++ ES6/8_ASYNCHRONOUS_PROG/index3.js
@@ -1,14 +1,7 @@
 console.clear();
-// Example of Asynchronous js
 
-// setTimeout() is an asynchronous function
-// tasks will be added in call stack, when one task is done then it will moved to the next one (LIFO)
-// setTimeout() will move the task into web APIs where the task will be running in background
-// other task will continue in call stack
-// when the call stack is completely empty then task (callback functions) from web APIs will be moved to task queue (FIFO)
-// when the entire call stack is empty then from task queue task will moved to call stack and the process is done
 
-console.log("--Example of Aynchronous Programming--");
+// Asynchronous Task Execution with Callbacks
 const taskOne = (callback) => {
   console.log(`Task One`);
   callback();
