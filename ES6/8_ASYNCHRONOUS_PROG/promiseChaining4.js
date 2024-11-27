c527591c1cd73995cff7bc4464f738f3adceddb4
web-collab <tanmoy587@gmail.com>
2024-11-27 09:08:13 +0530

    Promise Chaining wih Promise.race()

--- ES6/8_ASYNCHRONOUS_PROG/promiseChaining4.js
+++ ES6/8_ASYNCHRONOUS_PROG/promiseChaining4.js
@@ -12,12 +12,14 @@ const taskTwo = () =>{
         setTimeout(()=>{
             resolve("Task 2 is Completed");
             //reject("Task 2 is not Completed");  //If one reject is called the next task will not beexecuted
-        },3000)
+        },5000)
     })
 }
 const taskThree = () =>{
     return new Promise((resolve, reject)=>{
-        resolve("Task 3 is Completed");
+        setTimeout(()=>{
+            resolve("Task 3 is Completed");
+        },500)
     })
 }
 
@@ -26,12 +28,16 @@ const taskThree = () =>{
 // .then(taskThree).then((res)=>console.log(res))
 // .catch((err)=>console.log(err));
 
-Promise.all([taskOne, taskTwo]).then((res) => console.log(res));
-Promise.all([taskOne, taskTwo]).then(([res1,res2]) =>{
-    console.log(res1.name);
-    console.log(res2);
-}
-);
+// Promise.all([taskOne, taskTwo]).then((res) => console.log(res));
+// Promise.all([taskOne, taskTwo]).then(([res1,res2]) =>{
+//     console.log(res1.name);
+//     console.log(res2);
+// }
+// );
 
+
+Promise.race([taskTwo, taskThree]).then((result) =>
+  console.log(result)
+);
 
 console.log("ending the program");
