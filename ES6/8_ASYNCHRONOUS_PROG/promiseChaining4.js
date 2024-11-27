ca634a1e5a7b47a8d34708cafb61c04e957f087d
web-collab <tanmoy587@gmail.com>
2024-11-27 08:41:21 +0530

    Promise Chaining

---
+++ ES6/8_ASYNCHRONOUS_PROG/promiseChaining4.js
@@ -0,0 +1,25 @@
+//Promise Chaining
+
+console.log("starting the program");
+
+const taskOne = () =>{
+    return new Promise((resolve, reject)=>{
+        resolve("Task 1 is Completed");
+    })
+}
+const taskTwo = () =>{
+    return new Promise((resolve, reject)=>{
+        resolve("Task 2 is Completed");
+    })
+}
+const taskThree = () =>{
+    return new Promise((resolve, reject)=>{
+        resolve("Task 3 is Completed");
+    })
+}
+
+taskOne().then((res)=>console.log(res))
+.then(taskTwo).then((res)=>console.log(res))
+.then(taskThree).then((res)=>console.log(res));
+
+console.log("ending the program");
