53d2cff56947355fbbd5d1bbc61eaafef9f6366c
web-collab <tanmoy587@gmail.com>
2024-11-27 09:14:10 +0530

    Promise Chaining, async await

---
+++ ES6/8_ASYNCHRONOUS_PROG/promiseChaining5_with_asy_wait.js
@@ -0,0 +1,48 @@
+// promise, promise chaining, async await
+
+console.log("starting the program");
+
+const taskOne = () =>{
+    return new Promise((resolve, reject)=>{
+        resolve("Task 1 is Completed");
+    })
+}
+const taskTwo = () =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Task 2 is Completed");
+            //reject("Task 2 is not Completed");  //If one reject is called the next task will not beexecuted
+        },5000)
+    })
+}
+const taskThree = () =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Task 3 is Completed");
+        },500)
+    })
+}
+
+// taskOne().then((res)=>console.log(res))
+// .then(taskTwo).then((res)=>console.log(res))
+// .then(taskThree).then((res)=>console.log(res))
+// .catch((err)=>console.log(err));
+
+const callAllTasks = async () => {
+  try {
+    const t1 = await taskOne();
+    console.log(t1);
+    const t2 = await taskTwo();
+    console.log(t2);
+    const t3 = await taskThree();
+    console.log(t3);
+    const t4 = await taskFour();
+    console.log(t4);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+callAllTasks();
+
+console.log("ending the program");
