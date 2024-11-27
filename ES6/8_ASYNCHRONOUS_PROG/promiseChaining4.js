a2f7e599ee80efb16fe8c9c46c958f998dd00c31
web-collab <tanmoy587@gmail.com>
2024-11-27 09:03:05 +0530

    Promise Chaining will Promise.all()

--- ES6/8_ASYNCHRONOUS_PROG/promiseChaining4.js
+++ ES6/8_ASYNCHRONOUS_PROG/promiseChaining4.js
@@ -10,8 +10,8 @@ const taskOne = () =>{
 const taskTwo = () =>{
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            // resolve("Task 2 is Completed");
-            reject("Task 2 is not Completed");  //If one reject is called the next task will not beexecuted
+            resolve("Task 2 is Completed");
+            //reject("Task 2 is not Completed");  //If one reject is called the next task will not beexecuted
         },3000)
     })
 }
@@ -21,9 +21,17 @@ const taskThree = () =>{
     })
 }
 
-taskOne().then((res)=>console.log(res))
-.then(taskTwo).then((res)=>console.log(res))
-.then(taskThree).then((res)=>console.log(res))
-.catch((err)=>console.log(err));
+// taskOne().then((res)=>console.log(res))
+// .then(taskTwo).then((res)=>console.log(res))
+// .then(taskThree).then((res)=>console.log(res))
+// .catch((err)=>console.log(err));
+
+Promise.all([taskOne, taskTwo]).then((res) => console.log(res));
+Promise.all([taskOne, taskTwo]).then(([res1,res2]) =>{
+    console.log(res1.name);
+    console.log(res2);
+}
+);
+
 
 console.log("ending the program");
