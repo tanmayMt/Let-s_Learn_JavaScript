cb424c2c17ee39c70bd786563d0f42ca339eb88d
web-collab <tanmoy587@gmail.com>
2024-11-28 09:35:47 +0530

    call api using axios post

--- 14.API_Calling/scripts/axios.js
+++ 14.API_Calling/scripts/axios.js
@@ -38,4 +38,19 @@ const getData = () => {
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
-getData();
+// getData();
+
+const createData = () => {
+  makeRequest({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "post",
+    data: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+createData();
